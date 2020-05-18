cfe0dcb6f53d4bbd1f9a35f8d4516d0df839739e
Lance Chaney <lance.e.chaney@gmail.com>
2020-05-19 10:50:57 +1200

    In progress setup for NEAT controllers.

---
+++ Assets/Scripts/EvoAIController.cs
@@ -0,0 +1,57 @@
+﻿using SharpNeat.Phenomes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class EvoAIController : UnitController, InputController
+{
+    IBlackBox box;
+    bool isRunning;
+    float xInput;
+    float yInput;
+    HealthController health;
+
+    void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRunning)
+        {
+
+        }
+    }
+
+
+    public override void Activate(IBlackBox box)
+    {
+        this.box = box;
+        this.isRunning = true;
+
+        xInput = 0;
+        yInput = 0;
+    }
+
+    public override void Stop()
+    {
+        this.isRunning = false;
+    }
+
+    public override float GetFitness()
+    {
+        return health.health;
+    }
+
+    public float GetXInput()
+    {
+        return xInput;
+    }
+
+    public float GetYInput()
+    {
+        return yInput;
+    }
+}
--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class HealthController : MonoBehaviour
 {
+    public float health = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
